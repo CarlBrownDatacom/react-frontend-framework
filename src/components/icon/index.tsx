aef88cae332bdc85cd297afec2271672104a1144
MarlzyMarl <carl_brown111@hotmail.com>
2019-07-31 22:08:06 +1200

    added icons

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,17 @@
+import React, { lazy, Suspense } from 'react'; // importing FunctionComponent
+// import { StyledButton } from './index.style';
+
+interface IconProps {
+  name: string;
+}
+
+const Icon: React.FC<IconProps> = ({ name }: IconProps): React.ReactElement => {
+  const DynamicIcon = lazy((): Promise<any> => import(`../../assets/icons/${name}.tsx`));
+  return (
+    <Suspense fallback={<div>Loading ...</div>}>
+      <DynamicIcon />
+    </Suspense>
+  );
+};
+
+export default Icon;
